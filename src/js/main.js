ecf2c70992cdc2a37bbadb434e8502c7ff520727
Greg Schomburg <gschomburg@gmail.com>
2015-11-13 17:42:46 -0500

    added hull script

--- src/js/main.js
+++ src/js/main.js
@@ -6,32 +6,79 @@ $(function() {
 	console.log('window loaded');
     paper.setup('generator');
 	
-	var path = new Path();
-	path.strokeColor = 'black';
-	var start = new Point(100, 100);
-	path.moveTo(start);
-	path.lineTo(start.add([ 200, -50 ]));
+	// var path = new Path();
+	// path.strokeColor = 'black';
+	// var start = new Point(100, 100);
+	// path.moveTo(start);
+	// path.lineTo(start.add([ 200, -50 ]));
 
 	
 	view.viewSize = new Size(700, 700);
 
-	console.log(paper.project.getItem({class: Path}));
+	//console.log(paper.project.getItem({class: Path}));
 
-	// view.onFrame = function(event) {
-	// 	// On each frame, rotate the path by 3 degrees:
-	// 	path.rotate(3);
+	view.onFrame = function(event) {
+		// paper.project.clear();
+		// generate();
+		var paths = project.getItems({class: Path});
+		// console.log(paths);
+		for (var p=0; p<paths.length; p++){
+			paths[p].rotate(paths[p].rSpd);
 
-	// }
+		}
+	}
+	var tool = new Tool();
+	
+	tool.onMouseDown = function(event) {
+		console.log("bang");
+		paper.project.clear();
+		generate();
+	}
+	
+	generate();
+});
 
+var genericStyle = {
+	strokeColor: '#000000',
+	fillColor: '#FFFFFF',
+	strokeWidth: 2
+};
+
+
+
+function generate(){
 	//make stuff
-	for(var i=0; i<30; i++){
+	var previous;
+	for(var i=0; i<45; i++){
 		//newSpellParticle(Math.random() * view.viewSize.width, Math.random() * view.viewSize.height);
-		var t = triangle(100);
-		t.position = new Point(randomRange(100, view.viewSize.width - 100), randomRange(100, view.viewSize.height - 100));
+		// var t = triangle(100);
+		// t.position = new Point(randomRange(100, view.viewSize.width - 100), randomRange(100, view.viewSize.height - 100));
+
+		//var p = polygon(100, randomRange(4, 4));
+
+		var p = polygonPointCloud(100, 20, 90);
+		
+
+		// p.position = new Point(randomRange(-50, 50), randomRange(-50, 50));
+
+		
+		// if(previous!=null){
+		// 	previous.addChild(p)
+		// }
+		// previous = p;
+
+		// p.fillColor = new Color(Math.random(), Math.random(), Math.random());
+		p.simplify(.5);
+
+		p.rSpd = randomRange(-1, 1);
+		
+
+		p.position = new Point(randomRange(100, view.viewSize.width - 100), randomRange(100, view.viewSize.height - 100));
+		p.style = genericStyle;
 	}
 
 	view.draw();
-});
+}
 
 function newSpellParticle(_x, _y){
 	//make a part there
@@ -45,14 +92,52 @@ function newSpellParticle(_x, _y){
 
 function triangle(_radius){
 	var path = new Path();
-	//style?
-	path.strokeColor = 'black';
-	path.fillColor = 'white';
 	var start = new Point(randomRange(-_radius, _radius), randomRange(-_radius, _radius));
 	path.moveTo(start);
 	path.lineTo(new Point(randomRange(-_radius, _radius), randomRange(-_radius, _radius)));
 	path.lineTo(new Point(randomRange(-_radius, _radius), randomRange(-_radius, _radius)));
 	path.lineTo(start);
+	return path;
+}
+
+function polygon(_radius, _sides){
+	//generate points
+	var pts = [];
+	for(var p = 0; p<_sides; p++){
+		pts.push([randomRange(-_radius, _radius), randomRange(-_radius, _radius)]);
+	}
+	var verts = hull(pts);
+	var path = new Path();	
+	var start = new Point(verts[0][0], verts[0][1]);
+	path.moveTo(start);
+	for(var v=1; v<verts.length; v++){
+		path.lineTo(new Point(verts[v][0], verts[v][1]));
+
+	}
+	
+	//path.lineTo(new Point(randomRange(-_radius, _radius), randomRange(-_radius, _radius)));
+	path.lineTo(start);
+	return path;
+}
+
+function polygonPointCloud(_radius, _pointCount, _hullTolernace){
+	//generate points
+	var pts = [];
+	for(var p = 0; p<_pointCount; p++){
+		pts.push([randomRange(-_radius, _radius), randomRange(-_radius, _radius)]);
+	}
+	var verts = hull(pts, _hullTolernace);
+	var path = new Path();	
+	var start = new Point(verts[0][0], verts[0][1]);
+	path.moveTo(start);
+	for(var v=1; v<verts.length; v++){
+		path.lineTo(new Point(verts[v][0], verts[v][1]));
+
+	}
+	
+	//path.lineTo(new Point(randomRange(-_radius, _radius), randomRange(-_radius, _radius)));
+	// path.lineTo(start);
+	path.closed=true;
 	return path;
 }
 
